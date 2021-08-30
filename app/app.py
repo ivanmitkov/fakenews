400d0e5b80d29255d2f1c67a7ddc86bfa7b7bd0d
Ivan Mitkov <ivan.rmitkov@gmail.com>
2021-08-30 18:46:21 +0300

    building the docker file

---
+++ app/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+import re
+import pickle
+
+model=pickle.load(open('automl_model.pkl','rb'))
+tfidf=pickle.load(open('tfidf.pkl','rb'))
+
+# flask names
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/classify',methods=['POST'])
+def classify():
+      
+    if request.method == 'POST':    
+        query_title=request.form['news_title']
+        query_content=request.form['news_content']
+        total=query_title+query_content
+        total = re.sub('<[^>]*>', '', total)
+        total = re.sub(r'[^\w\s]','', total)
+        total = total.lower()     
+        data=[total]
+        vect=tfidf.transform(data).toarray()
+        pred=model.predict(vect)
+    
+    return render_template('index.html', prediction_text='The news is : {}'.format(pred[0]))
+    
+if __name__=="__main__":
+    app.run(debug=True)
+    
